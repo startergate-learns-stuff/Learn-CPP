5aff421432fa6789778d6e9731914be4777cc735
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-03-05 17:37:36 +0900

    3.5

---
+++ src1.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	//string str;
+	//cin >> str; 
+	//std::cout << "Hello, " << str << std::endl;
+	std::cout << 10 + 20 << 30 << std::endl;
+	// std::cout << 10U << std::endl;
+	int a = 10;
+	int b(10);
+	int c(b);
+	cout << a + b + c << endl;
+	return 1;
+}
